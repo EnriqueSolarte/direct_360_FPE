dae99c6b81824fc8dc06a4c05110f88f395a4162
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2023-01-29 16:56:56 +0800

    Add list of google-drive IDS for the entire MP3D-FPE dataset

--- download_mp3d_fpe.py
+++ download_mp3d_fpe.py
@@ -14,7 +14,7 @@ def download_scenes(opt):
     list_google_scenes = opt.ids_file
     scenes_ids = pd.read_csv(list_google_scenes)
 
-    for zip_fn, gd_id in zip(scenes_ids.Id, scenes_ids.Path):
+    for gd_id, zip_fn in zip(scenes_ids.Id, scenes_ids.Path):
         print(f"Downloading... {zip_fn}")
         url = f"https://drive.google.com/uc?id={gd_id}"
         output_file = os.path.join(output_dir, zip_fn)
@@ -24,7 +24,7 @@ def download_scenes(opt):
 def get_passed_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--output_dir', type=str, default="./dataset", help='Output dataset directory...')
-    parser.add_argument('--ids_file', type=str, default="./data/pilot_scenes_google_ids.csv", help="lists of IDS to download from GoogleDrive")
+    parser.add_argument('--ids_file', type=str, default="./data/mp3d_fpe_google_drive_ids.csv", help="lists of IDS to download from GoogleDrive")
     opt = parser.parse_args()
     return opt
 
