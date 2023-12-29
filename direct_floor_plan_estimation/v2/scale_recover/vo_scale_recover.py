bd5d82700a3c154074b8f1ee6e84203a01d3800d
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2023-12-29 09:00:08 +0800

    for fpe.v2 add maskingmask_noisy_points() to avoid extreme noisy layout estimation

--- direct_floor_plan_estimation/v2/scale_recover/vo_scale_recover.py
+++ direct_floor_plan_estimation/v2/scale_recover/vo_scale_recover.py
@@ -23,7 +23,6 @@ class VO_ScaleRecover:
                        list_ly,
                        vis_plot_dir=None):
         self.reset_all()
-        flag_masking_points = False
         for batch in tqdm(range(0, list_ly.__len__(), self.sliding_windows//2), desc="Running VO Scale Recovery"):
             _list_ly = list_ly[batch:batch + self.sliding_windows]
             # * setting min and max scale as initial scale value
@@ -37,12 +36,10 @@ class VO_ScaleRecover:
                 self.hist_scale = []
                 scale_step = c2f
                 # list_ly x boundary_floor.shape[1]
-                number_points = _list_ly.__len__()*_list_ly[0].boundary_floor.shape[1]
-                mask_points = np.ones((number_points,), dtype=bool)
                 while True:
                     # ! Applying scale
                     pcl = apply_vo_scale(scale=scale, list_ly=_list_ly)
-                    pcl = pcl[:, mask_points]
+                    pcl = mask_noisy_points(_list_ly)
                     # ! Computing Entropy
                     h, ocg_map = compute_entropy_from_pcl(
                         pcl=pcl, grid_size=self.grid_size, 
@@ -50,12 +47,7 @@ class VO_ScaleRecover:
 
                     # print(ocg_map.shape, ocg_map.size)
                     if ocg_map.size > np.prod(self.max_ocg_map_size):
-                        print("Ocg map size is too large")
-                        distances = np.hstack([ly.cam2boundary for ly in _list_ly])
-                        mask_points = distances < np.median(distances) * 2
-                        print("Masking points", mask_points.shape, np.sum(mask_points))       
-                        flag_masking_points = True
-                        continue                    
+                        raise ValueError("Ocg map size is too large")
                     
                     self.hist_entropy.append(h)
                     self.hist_scale.append(scale)
@@ -81,16 +73,9 @@ class VO_ScaleRecover:
         best_vo_scale = self.compute_best_vo_scale()
         apply_vo_scale(scale=best_vo_scale, list_ly=list_ly)
         if vis_plot_dir is not None:
-            fn = os.path.join(vis_plot_dir, f"scale_recover.png")
-            if flag_masking_points:
-                print("Ocg map size is too large")
-                distances = np.hstack([ly.cam2boundary for ly in list_ly])
-                mask_points = distances < np.median(distances) * 2
-                print("Masking points", mask_points.shape, np.sum(mask_points))       
-            else:
-                mask_points = None
-            plot_scale_recover_vis(list_ly, best_vo_scale, self.grid_size, fn, mask_points, hist_scale=self.hist_best_scale, hist_entropy=self.hist_best_entropy)
-        
+            fn = os.path.join(vis_plot_dir, f"scale_recover.png")                
+            plot_scale_recover_vis(list_ly, best_vo_scale, self.grid_size, fn, hist_scale=self.hist_best_scale, hist_entropy=self.hist_best_entropy)
+            
     def compute_best_vo_scale(self):
         # idx_min = np.argmin(self.hist_best_entropy)
         # best_vo_scale = self.hist_best_scale[idx_min]
@@ -162,7 +147,7 @@ def apply_vo_scale(scale, list_ly):
     return np.hstack([ly.boundary_floor for ly in list_ly])
 
 
-def plot_scale_recover_vis(list_ly, scale, grid_size, fn, mask_points=None, hist_scale=None, hist_entropy=None):
+def plot_scale_recover_vis(list_ly, scale, grid_size, fn, hist_scale=None, hist_entropy=None):
     
     if hist_scale is None:
         hist_scale = list(np.ones((10)) * scale)
@@ -170,13 +155,13 @@ def plot_scale_recover_vis(list_ly, scale, grid_size, fn, mask_points=None, hist
         hist_entropy = list(np.ones((10)) * scale)
         
     # ! Computing Entropy Plot
-    pcl = apply_vo_scale(scale=scale, list_ly=list_ly)
-    if mask_points is not None:
-        pcl = pcl[:, mask_points] 
+    apply_vo_scale(scale=scale, list_ly=list_ly)
+    pcl = mask_noisy_points(list_ly)
     _, ocg_map = compute_entropy_from_pcl(
     pcl=pcl, grid_size=grid_size, 
     return_ocg_map=True)    
     ocg_map = ocg_map / np.max(ocg_map)
+    
     fig = plt.figure("Optimization", figsize=(10, 4))
     plt.clf()
     ax1 = fig.add_subplot(121)
@@ -196,3 +181,10 @@ def plot_scale_recover_vis(list_ly, scale, grid_size, fn, mask_points=None, hist
     plt.draw()
     plt.waitforbuttonpress(0.01)
     plt.savefig(fn)
+
+def mask_noisy_points(list_ly):
+    distances = np.hstack([ly.cam2boundary for ly in list_ly])
+    mask_points = distances < np.median(distances) * 2
+    pcl = np.hstack([ly.boundary_floor for ly in list_ly])
+    pcl = pcl[:, mask_points]
+    return pcl
